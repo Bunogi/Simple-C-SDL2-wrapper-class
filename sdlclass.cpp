589e70e0ab3c9c775423a16ac622452d57306dac
Bunogi <haakon.jordet@gmail.com>
2016-01-17 20:03:11 +0100

    Added new deltatime and new drawing functions

--- sdlclass.cpp
+++ sdlclass.cpp
@@ -25,6 +25,7 @@ SDL::SDL(const int windowWidth, const int windowHeight, const std::string& windo
 
 	SDL_GetWindowSize(window, &width, &height);
 	renderClearColour = {0x3F, 0x3F, 0x3F, 0xFF};
+	lastTime = thisTime = 0;
 }
 
 SDL::~SDL() {
@@ -32,18 +33,33 @@ SDL::~SDL() {
 	SDL_DestroyWindow(window);
 }
 
-void SDL::drawRectFill(const SDL_Rect& rect, const SDL_Colour& colour) const {
+Uint32 SDL::getDeltaTime() {
+	thisTime = SDL_GetTicks();
+	Uint32 dt = thisTime - lastTime;
+	lastTime = thisTime;
+	return dt;
+}
+
+void SDL::setRenderColour(const SDL_Colour& colour) const {
 	if (SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, colour.a) < 0) 
 		throw SDLException("SDL_SetRenderDrawColor");
+}
+
+void SDL::drawRectFill(const SDL_Rect& rect, const SDL_Colour& colour) const {
+	setRenderColour(colour);
 	if (SDL_RenderFillRect(renderer, &rect) < 0) 
 		throw SDLException("SDL_RenderFillRect");
 }
 
 void SDL::drawRectOutline(const SDL_Rect& rect, const SDL_Colour& colour) const {
-	if (SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, colour.a) < 0) 
-		throw SDLException("SDL_SetRenderDrawColor");
+	setRenderColour(colour);
 	if (SDL_RenderDrawRect(renderer, &rect) < 0) 
 		throw SDLException("SDL_RenderFillRect");
+}
+
+void SDL::drawLine(const int x1, const int y1, const int x2, const int y2, const SDL_Colour& colour) const {
+	setRenderColour(colour);
+	SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
 }
 
 void SDL::renderPresent() const {
--- sdlclass.hpp
+++ sdlclass.hpp
@@ -1,5 +1,6 @@
 #ifndef SDLCLASS_HPP
 #define SDLCLASS_HPP
+#pragma once
 
 #include <stdexcept>
 #include <cstdint>
@@ -13,14 +14,20 @@ class SDL {
 	SDL_Colour renderClearColour;
 	int width;
 	int height;
+	Uint32 lastTime;
+	Uint32 thisTime;
+
+	void setRenderColour(const SDL_Colour& colour) const;
 	public:
 		SDL(const int windowWidth, const int windowHeight, const std::string& windowName="Window", std::uint32_t windowFlags=SDL_WINDOW_SHOWN, std::uint32_t renderFlags=SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 		~SDL();
 		void drawRectFill(const SDL_Rect& rect, const SDL_Colour& colour = {0xFF, 0xFF, 0xFF, 0xFF}) const;
 		void drawRectOutline(const SDL_Rect& rect, const SDL_Colour& colour = {0xFF, 0xFF, 0xFF, 0xFF}) const;
+		void drawLine(const int x1, const int y1, const int x2, const int y2, const SDL_Colour& colour = {0xFF, 0xFF, 0xFF, 0xFF}) const;
 		void renderPresent() const;
 		void clearRenderer() const;
 		void setRenderClearColour(const SDL_Colour& colour);
+		Uint32 getDeltaTime();
 		int getWidth() {
 			return width;
 		}
