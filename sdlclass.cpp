731b4007e5dc84df7128fc5c6ed424e2a5378ef3
Bunogi <haakon.jordet@gmail.com>
2016-01-17 16:54:10 +0100

    Initial commit

---
+++ sdlclass.cpp
@@ -0,0 +1,55 @@
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+#include <SDL2/SDL.h>
+
+#include "sdlclass.hpp"
+
+SDL::SDL(const int windowWidth, const int windowHeight, const std::string& windowName, std::uint32_t windowFlags, std::uint32_t renderFlags) {
+	window = nullptr;
+	renderer = nullptr;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		throw SDLException("SDL_Init");
+	
+	window = SDL_CreateWindow(windowName.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, windowFlags);
+	if (window == nullptr)
+		throw SDLException("SDL_CreateWindow");
+
+	renderer = SDL_CreateRenderer(window, -1, renderFlags);
+	if (renderer == nullptr) {
+		SDL_DestroyWindow(window);
+		throw SDLException("SDL_CreateRenderer");
+	}
+
+	SDL_GetWindowSize(window, &width, &height);
+}
+
+SDL::~SDL() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+}
+
+void SDL::drawRectFill(const SDL_Rect& rect, const SDL_Colour& colour) const {
+	if (SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, colour.a) < 0) 
+		throw SDLException("SDL_SetRenderDrawColor");
+	if (SDL_RenderFillRect(renderer, &rect) < 0) 
+		throw SDLException("SDL_RenderFillRect");
+}
+
+void SDL::drawRectOutline(const SDL_Rect& rect, const SDL_Colour& colour) const {
+	if (SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, colour.a) < 0) 
+		throw SDLException("SDL_SetRenderDrawColor");
+	if (SDL_RenderDrawRect(renderer, &rect) < 0) 
+		throw SDLException("SDL_RenderFillRect");
+}
+
+void SDL::renderPresent() const {
+	SDL_RenderPresent(renderer);
+}
+
+void SDL::clearRenderer() const {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
+}
---
+++ sdlclass.hpp
@@ -0,0 +1,34 @@
+#ifndef SDLCLASS_HPP
+#define SDLCLASS_HPP
+
+#include <stdexcept>
+#include <cstdint>
+#include <string>
+
+#include <SDL2/SDL.h>
+
+class SDL {
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	int width;
+	int height;
+	public:
+		SDL(const int windowWidth, const int windowHeight, const std::string& windowName="Window", std::uint32_t windowFlags=SDL_WINDOW_SHOWN, std::uint32_t renderFlags=SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		~SDL();
+		void drawRectFill(const SDL_Rect& rect, const SDL_Colour& colour = {0xFF, 0xFF, 0xFF, 0xFF}) const;
+		void drawRectOutline(const SDL_Rect& rect, const SDL_Colour& colour = {0xFF, 0xFF, 0xFF, 0xFF}) const;
+		void renderPresent() const;
+		void clearRenderer() const;
+		int getWidth() {
+			return width;
+		}
+		int getHeight() {
+			return height;
+		}
+};
+
+class SDLException : public std::runtime_error {
+	using std::runtime_error::runtime_error;
+};
+
+#endif
